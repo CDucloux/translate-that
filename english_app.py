f9a0eb85c72ba17cc46e43489786cda4c51263db
CDucloux <corentin_ducloux@outlook.fr>
2023-11-28 20:41:58 +0100

    Ajout de la `feature` TRAINING SESSION

--- english_app.py
+++ english_app.py
@@ -4,63 +4,75 @@ from annotated_text import annotated_text
 from components.app_components import *
 
 
-def progress_bar_timer():
-    # Bouton pour démarrer le timer
-    # Par défaut, configurer le timer pour avoir 20 secondes par mot.
-    col1, col2 = st.columns([1, 1])
-    success_container = st.empty()
-    with col1:
-        if st.button("Démarrer le timer"):
-            # Barre de progression pendant 2 minutes
-            with col2:
-                progress_bar = st.progress(0)
-                timer_text = st.empty()
-
-                for i in range(121):  # 2 minutes = 120 secondes
-                    # Mettre à jour la barre de progression et le timer toutes les secondes
-                    progress_bar.progress(i / 120)
-                    timer_text.text(f"Temps restant : {120 - i} secondes")
-                    time.sleep(1)
-                success_container.success("⏲ La session d'apprentissage est terminée.")
-
-
-# with st.sidebar:
-#    progress_bar_timer()
-
-
 def main():
     page_config()
     remove_white_space()
+    import_fontawesome()
     selected_language = select_language()
-    # nlp_fr = load_fr_model()
-    # nlp_en = load_en_model()
+    display_author(selected_language)
     set_header(selected_language)
     uploaded_file = upload_file(selected_language)
     set_title(selected_language)
 
     data = load_data(uploaded_file)
     text_placeholder(data, selected_language)
-    mode = select_mode(data, selected_language)
+    check = check_columns(data)
+    if check:
+        mode = select_mode(data, selected_language)
 
-    # Affichage en fonction du mode sélectionné
-    if mode == "📚 Apprentissage" or mode == "📚 Learning":
-        display_data(data, selected_language)
+        # Affichage en fonction du mode sélectionné
+        if mode == "📚 Apprentissage" or mode == "📚 Learning":
+            display_data(data, selected_language)
 
-    elif mode == "🎲 Texte aléatoire" or mode == "🎲 Random text":
-        user_translation, submit, clear = random_text(data, selected_language)
-        if submit:
-            post_submit(selected_language, user_translation, "test_1", "test_2")
-            # dans le post_submit, remplacer "test_1" par nlp_fr et nlp_en
-        if clear:
-            # Réinitialise les mots aléatoires
-            (
-                st.session_state.random_word,
-                st.session_state.correct_translation,
-            ) = get_random_word(data, selected_language)
-            st.rerun()
+        elif mode == "🎲 Texte aléatoire" or mode == "🎲 Random text":
+            user_translation, submit, clear = random_text(data, selected_language)
+            if submit:
+                post_submit(selected_language, user_translation)
+            if clear:
+                # Réinitialise les mots aléatoires
+                (
+                    st.session_state.random_word,
+                    st.session_state.correct_translation,
+                ) = get_random_word(data, selected_language)
+                st.rerun()
 
-    elif mode == "🏋️‍♂️ Entraînement" or mode == "🏋️‍♂️ Training session":
-        st.error("⏰ En cours de développement ")
+        elif mode == "🏋️‍♂️ Entraînement" or mode == "🏋️‍♂️ Training session":
+            if "index" not in st.session_state:
+                st.session_state.index = 0
+            if st.session_state.index >= len(data):
+                st.session_state.index = 0
+            if "score" not in st.session_state:
+                st.session_state.score = 0
+            if st.session_state.index == 0:
+                st.session_state.score = 0
+            check_session_state_train(data, selected_language)
+            train_text_subheader(selected_language)
+            user_translation_train, submit_train, clear_train = userform_train(
+                selected_language
+            )
+            if submit_train:
+                post_submit_train(selected_language, user_translation_train)
+            index_writer_train(data, selected_language)
+            score_writer_train(selected_language)
+            if clear_train:
+                # va chercher un nouveau mot
+                (
+                    st.session_state.next_word,
+                    st.session_state.correct_translation_train,
+                ) = get_next_word(data, selected_language)
+                st.rerun()
+            if st.session_state.index == len(data) - 1:
+                final_scorer(data)
+    else:
+        if data is not None:
+            if selected_language == "FR":
+                st.write(
+                    "> Les colonnes **French** ou **English** n'ont pas été trouvées dans le dataset."
+                )
+            else:
+                st.write(
+                    "> Sadly, no column **French** or **English** has been found in the dataset."
+                )
 
 
 if __name__ == "__main__":
